a0eec3ce7d925ec73a3472975161cb9f158857f5
Eric Oliveira <oiericoi@hotmail.com>
2023-11-11 12:59:33 -0300

    feat: add stream model

---
+++ models/stream.go
@@ -0,0 +1,13 @@
+package models
+
+import "time"
+
+type Stream struct {
+	ID              int64            `db:"id,autoincr"`
+	Name            string           `db:"name,unique"`
+	ImageUrl        string           `db:"image_url"`
+	IsLive          bool             `db:"is_live"`
+	DiscordChannels []DiscordChannel `db:"-"`
+	CreatedAt       time.Time        `db:"created_at"`
+	UpdatedAt       time.Time        `db:"updated_at"`
+}
