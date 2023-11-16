1d044211c55c581ace673342460120cf33a99793
Eric Oliveira <oiericoi@hotmail.com>
2023-11-16 10:47:10 -0300

    refactor: modify user repository

--- repository/user_repository.go
+++ repository/user_repository.go
@@ -9,6 +9,7 @@ import (
 
 type IUserRepository interface {
 	Create(ctx context.Context, user *models.User) error
+	GetByEmail(ctx context.Context, email string) (*models.User, error)
 }
 
 type UserRepository struct {
@@ -22,9 +23,9 @@ func NewUserRepository(db *sqlx.DB) *UserRepository {
 func (ur *UserRepository) Create(ctx context.Context, user *models.User) error {
 	query := `
 	INSERT INTO users 
-		(id, username, email, channel_limit, created_at, updated_at) 
+		(id, discord_id, username, email, avatar, channel_limit, created_at, updated_at) 
 	VALUES 
-		(:id, :username, :email, :channel_limit, :created_at, :updated_at)
+		(:id, :discord_id, :username, :email, :avatar, :channel_limit, :created_at, :updated_at)
 	`
 
 	_, err := ur.DB.NamedExecContext(ctx, query, user)
@@ -33,4 +34,16 @@ func (ur *UserRepository) Create(ctx context.Context, user *models.User) error {
 	}
 
 	return nil
+}
+
+func (ur *UserRepository) GetByEmail(ctx context.Context, email string) (*models.User, error) {
+	var user models.User
+
+	query := "SELECT * FROM users WHERE email = $1"
+	err := ur.DB.GetContext(ctx, &user, query, email)
+	if err != nil {
+		return &models.User{}, err
+	}
+
+	return &user, nil
 }
