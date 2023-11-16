3a35029712be10c56ec4e1c8933d15db696d5522
Eric Oliveira <oiericoi@hotmail.com>
2023-11-16 10:46:21 -0300

    refactor: modify user model

--- models/user.go
+++ models/user.go
@@ -8,8 +8,10 @@ import (
 
 type User struct {
 	ID           uuid.UUID `db:"id,uuid,pk"`
+	DiscordID    string    `db:"discord_id,unique"`
 	Username     string    `db:"username"`
 	Email        string    `db:"email,unique"`
+	Avatar       string    `db:"avatar"`
 	ChannelLimit int       `db:"channel_limit"`
 	CreatedAt    time.Time `db:"created_at"`
 	UpdatedAt    time.Time `db:"updated_at"`
