fb6d165e99317049b8fb3b657952a38a245780da
Eric Oliveira <oiericoi@hotmail.com>
2023-11-16 11:29:58 -0300

    feat: add auth routes in app

--- app.go
+++ app.go
@@ -2,13 +2,21 @@ package application
 
 import (
 	"log"
+	"net/http"
 
+	"github.com/ericoliveiras/alert-bot-go/auth"
 	"github.com/ericoliveiras/alert-bot-go/config"
 	"github.com/ericoliveiras/alert-bot-go/server"
 )
 
 func Start(config *config.Config) {
 	app := server.NewServer(config)
+
+	http.HandleFunc("/", auth.HandleMain)
+	http.HandleFunc("/login", auth.HandleLogin)
+	http.HandleFunc("/callback", func(w http.ResponseWriter, r *http.Request) {
+		auth.HandleCallback(w, r, app.DB)
+	})
 
 	err := app.Start(config.HTTP.Port)
 	if err != nil {
