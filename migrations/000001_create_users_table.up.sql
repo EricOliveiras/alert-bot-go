2f32da723a9bb7bd87a6d26ea934932735c5729f
Eric Oliveira <oiericoi@hotmail.com>
2023-11-16 10:46:41 -0300

    refactor: modify user migration

--- migrations/000001_create_users_table.up.sql
+++ migrations/000001_create_users_table.up.sql
@@ -1,8 +1,9 @@
 CREATE TABLE IF NOT EXISTS users (
   id UUID PRIMARY KEY,
+  discord_id TEXT UNIQUE,
   username TEXT,
   email TEXT UNIQUE,
-  password TEXT,
+  avatar TEXT,
   channel_limit INT,
   created_at TIMESTAMPTZ,
   updated_at TIMESTAMPTZ
