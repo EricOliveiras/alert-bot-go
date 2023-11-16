0acb0bf58038710132b57acc3de2ed25689a519b
Eric Oliveira <oiericoi@hotmail.com>
2023-11-16 10:48:36 -0300

    refactor: modify discord config

--- config/discord.go
+++ config/discord.go
@@ -3,11 +3,23 @@ package config
 import "os"
 
 type DiscordConfig struct {
-	Token string
+	Token        string
+	ClientID     string
+	ClientSecret string
+	AuthURL      string
+	TokenURL     string
+	RedirectURL  string
+	Scopes       []string
 }
 
 func LoadDiscordConfig() DiscordConfig {
 	return DiscordConfig{
-		Token: os.Getenv("DISCORD_TOKEN"),
+		Token:        os.Getenv("DISCORD_TOKEN"),
+		ClientID:     os.Getenv("DISCORD_CLIENT_ID"),
+		ClientSecret: os.Getenv("DISCORD_CLIENT_SECRET"),
+		AuthURL:      os.Getenv("DISCORD_AUTH_URL"),
+		TokenURL:     os.Getenv("DISCORD_AUTH_TOKEN"),
+		RedirectURL:  os.Getenv("DISCORD_URL_REDIRECT"),
+		Scopes:       []string{"identify", "email"},
 	}
 }
