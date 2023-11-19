8321b0be07ae723bf56cceddae85cb6e3a6bbb3a
Eric Oliveira <oiericoi@hotmail.com>
2023-11-19 13:49:18 -0300

    feat: add auth middleware

---
+++ middleware/auth_middleware.go
@@ -0,0 +1,26 @@
+package middleware
+
+import (
+	"net/http"
+	"time"
+
+	"golang.org/x/oauth2"
+)
+
+func IsAuthenticated(r *http.Request) bool {
+	cookie, err := r.Cookie("access_token")
+	if err != nil {
+		return false
+	}
+
+	token := &oauth2.Token{
+		AccessToken: cookie.Value,
+	}
+
+	if !token.Valid() {
+		return false
+	}
+
+	expiryTime := token.Expiry
+	return expiryTime.Before(time.Now())
+}
