e97892a0e8140922061c95e62d01178153da6f4a
Eric Oliveira <oiericoi@hotmail.com>
2023-11-19 13:48:13 -0300

    refactor: modify discord config

--- config/discord.go
+++ config/discord.go
@@ -3,23 +3,27 @@ package config
 import "os"
 
 type DiscordConfig struct {
-	Token        string
-	ClientID     string
-	ClientSecret string
-	AuthURL      string
-	TokenURL     string
-	RedirectURL  string
-	Scopes       []string
+	Token            string
+	ClientID         string
+	ClientSecret     string
+	AuthURL          string
+	TokenURL         string
+	RedirectURL      string
+	GetUserInfoUrl   string
+	GetGuildsInfoUrl string
+	Scopes           []string
 }
 
 func LoadDiscordConfig() DiscordConfig {
 	return DiscordConfig{
-		Token:        os.Getenv("DISCORD_TOKEN"),
-		ClientID:     os.Getenv("DISCORD_CLIENT_ID"),
-		ClientSecret: os.Getenv("DISCORD_CLIENT_SECRET"),
-		AuthURL:      os.Getenv("DISCORD_AUTH_URL"),
-		TokenURL:     os.Getenv("DISCORD_AUTH_TOKEN"),
-		RedirectURL:  os.Getenv("DISCORD_URL_REDIRECT"),
-		Scopes:       []string{"identify", "email"},
+		Token:            os.Getenv("DISCORD_TOKEN"),
+		ClientID:         os.Getenv("DISCORD_CLIENT_ID"),
+		ClientSecret:     os.Getenv("DISCORD_CLIENT_SECRET"),
+		AuthURL:          os.Getenv("DISCORD_AUTH_URL"),
+		TokenURL:         os.Getenv("DISCORD_AUTH_TOKEN"),
+		RedirectURL:      os.Getenv("DISCORD_URL_REDIRECT"),
+		GetUserInfoUrl:   os.Getenv("USER_INFO_URL"),
+		GetGuildsInfoUrl: os.Getenv("GUILD_INFO_URL"),
+		Scopes:           []string{"identify", "email", "guilds"},
 	}
 }
