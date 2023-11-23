08695ba06668634fda1f60b0c8fac99a8d9fe8d0
Eric Oliveira <oiericoi@hotmail.com>
2023-11-23 12:38:19 -0300

    feat: add discord router

---
+++ router/discord_router.go
@@ -0,0 +1,13 @@
+package router
+
+import (
+	"net/http"
+
+	"github.com/ericoliveiras/alert-bot-go/handler"
+	"github.com/jmoiron/sqlx"
+)
+
+func SetupDiscordRoutes(db *sqlx.DB) {
+	discordHandler := handler.NewDiscordHandler(db)
+	http.HandleFunc("/discord-channel/create", discordHandler.Create)
+}
