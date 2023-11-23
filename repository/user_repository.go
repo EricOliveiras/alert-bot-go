bff923b89fe215273d838e6668b4a617214daa9b
Eric Oliveira <oiericoi@hotmail.com>
2023-11-23 10:32:19 -0300

    feat: add get user by discord id and update channel limit methods

--- repository/user_repository.go
+++ repository/user_repository.go
@@ -4,12 +4,15 @@ import (
 	"context"
 
 	"github.com/ericoliveiras/alert-bot-go/models"
+	"github.com/google/uuid"
 	"github.com/jmoiron/sqlx"
 )
 
 type IUserRepository interface {
 	Create(ctx context.Context, user *models.User) error
 	GetByEmail(ctx context.Context, email string) (*models.User, error)
+	GetByDiscordID(ctx context.Context, discordId string) (*models.User, error)
+	UpdateChannelLimit(ctx context.Context, userId uuid.UUID, limit int) error
 }
 
 type UserRepository struct {
@@ -46,4 +49,27 @@ func (ur *UserRepository) GetByEmail(ctx context.Context, email string) (*models
 	}
 
 	return &user, nil
+}
+
+func (ur *UserRepository) GetByDiscordID(ctx context.Context, discordId string) (*models.User, error) {
+	var user models.User
+
+	query := "SELECT * FROM users WHERE discord_id = $1"
+	err := ur.DB.GetContext(ctx, &user, query, discordId)
+	if err != nil {
+		return &models.User{}, err
+	}
+
+	return &user, nil
+}
+
+func (ur *UserRepository) UpdateChannelLimit(ctx context.Context, userId uuid.UUID, limit int) error {
+	query := "UPDATE users SET channel_limit = $1 WHERE id = $2"
+
+	_, err := ur.DB.ExecContext(ctx, query, limit, userId)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
