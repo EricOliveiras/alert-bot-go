6544f6740fdd64238993f548fe50c490e37d07c0
Eric Oliveira <oiericoi@hotmail.com>
2023-11-25 12:10:16 -0300

    refactor: modify user response

--- response/user_response.go
+++ response/user_response.go
@@ -1,8 +1,28 @@
 package response
 
-import "github.com/ericoliveiras/alert-bot-go/request"
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type UserResponse struct {
+	ID           uuid.UUID `json:"id"`
+	DiscordID    string    `json:"discord_id"`
+	Username     string    `json:"username"`
+	Email        string    `json:"email"`
+	Avatar       string    `json:"avatar"`
+	ChannelLimit int       `json:"channel_limit"`
+	CreatedAt    time.Time `json:"created_at"`
+	UpdatedAt    time.Time `json:"updated_at"`
+}
 
 type UserGuildsResponse struct {
-	User   request.CreateUser `json:"user"`
-	Guilds []GuildResponse    `json:"guilds"`
+	User   UserResponse    `json:"user"`
+	Guilds []GuildResponse `json:"guilds"`
+}
+
+type UserChannelResponse struct {
+	User    UserResponse    `json:"user"`
+	Channel ChannelResponse `json:"channel"`
 }
