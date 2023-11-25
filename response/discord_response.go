91bb057755249d9bdb61df85a79934730143148a
Eric Oliveira <oiericoi@hotmail.com>
2023-11-25 12:10:26 -0300

    refactor: modify discord response

--- response/discord_response.go
+++ response/discord_response.go
@@ -1,5 +1,11 @@
 package response
 
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
 type GuildResponse struct {
 	Icon     string `json:"icon"`
 	ID       string `json:"id"`
@@ -12,4 +18,15 @@ type GuildChannelsResponse struct {
 	ID   string `json:"id"`
 	Name string `json:"name"`
 	Type int    `json:"type"`
+}
+
+type ChannelResponse struct {
+	ID          uuid.UUID `json:"id"`
+	Name        string    `json:"name"`
+	ChannelId   string    `json:"channel_id"`
+	ServerId    string    `json:"server_id"`
+	StreamLimit int       `json:"stream_limit"`
+	UserID      uuid.UUID `json:"user_id"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
 }
