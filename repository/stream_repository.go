3150aae89c3f6b94f5487bb0c75f0aea927687f4
Eric Oliveira <oiericoi@hotmail.com>
2023-12-05 11:33:17 -0300

    refactor: modify stream repository

--- repository/stream_repository.go
+++ repository/stream_repository.go
@@ -10,6 +10,9 @@ import (
 type IStreamRepository interface {
 	Create(ctx context.Context, stream *models.Stream) (*models.Stream, error)
 	GetByStreamName(ctx context.Context, name string) (*models.Stream, error)
+	GetByStreamID(ctx context.Context, id int) (*models.Stream, error)
+	GetAllStreams(ctx context.Context) ([]models.Stream, error)
+	UpdateStreamIsLive(ctx context.Context, id int, isLive bool) error
 }
 
 type StreamRepository struct {
@@ -54,4 +57,39 @@ func (sr *StreamRepository) GetByStreamName(ctx context.Context, name string) (*
 	}
 
 	return &stream, nil
+}
+
+func (sr *StreamRepository) GetByStreamID(ctx context.Context, id int) (*models.Stream, error) {
+	var stream models.Stream
+
+	query := "SELECT * FROM streams WHERE id = $1"
+	err := sr.DB.GetContext(ctx, &stream, query, id)
+	if err != nil {
+		return &models.Stream{}, err
+	}
+
+	return &stream, nil
+}
+
+func (sr *StreamRepository) GetAllStreams(ctx context.Context) ([]models.Stream, error) {
+	var streams []models.Stream
+
+	query := "SELECT id, name, image_url, is_live, created_at, updated_at FROM streams"
+	err := sr.DB.SelectContext(ctx, &streams, query)
+	if err != nil {
+		return nil, err
+	}
+
+	return streams, nil
+}
+
+func (sr *StreamRepository) UpdateStreamIsLive(ctx context.Context, id int, isLive bool) error {
+	query := "UPDATE streams SET is_live = $1 WHERE id = $2"
+
+	_, err := sr.DB.ExecContext(ctx, query, isLive, id)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
