5d9952f4f874524a7f9f880f74f1261dc16ef7bd
Eric Oliveira <oiericoi@hotmail.com>
2023-12-05 11:31:43 -0300

    chore: modify discord channel streams table sql migration

--- migrations/000004_create_discord_channel_streams_table.up.sql
+++ migrations/000004_create_discord_channel_streams_table.up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS discord_channel_streams (
   id SERIAL PRIMARY KEY,
-  discord_channel_id UUID REFERENCES discord_channels(id),
+  discord_channel_id TEXT REFERENCES discord_channels(channel_id),
   stream_id BIGINT REFERENCES streams(id)
 );
