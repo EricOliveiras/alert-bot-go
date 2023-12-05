01fd43172c83a58f1b1871d752942366f8632ec6
Eric Oliveira <oiericoi@hotmail.com>
2023-12-05 11:32:17 -0300

    refactor: modify discord channel stream model

--- models/discord_channel_stream.go
+++ models/discord_channel_stream.go
@@ -1,10 +1,6 @@
 package models
 
-import (
-	"github.com/google/uuid"
-)
-
 type DiscordChannelStream struct {
-	DiscordChannelID uuid.UUID `db:"discord_channel_id,fk"`
-	StreamID         int       `db:"stream_id,fk"`
+	DiscordChannelID string `db:"discord_channel_id,fk"`
+	StreamID         int    `db:"stream_id,fk"`
 }
