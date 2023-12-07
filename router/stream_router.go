4f75f7ad21bd461b8ce2717040b5f22797eb2cd0
Eric Oliveira <oiericoi@hotmail.com>
2023-12-07 13:42:07 -0300

    refactor: modify stream router

--- router/stream_router.go
+++ router/stream_router.go
@@ -3,11 +3,12 @@ package router
 import (
 	"net/http"
 
+	"github.com/bwmarrin/discordgo"
 	"github.com/ericoliveiras/alert-bot-go/handler"
 	"github.com/jmoiron/sqlx"
 )
 
-func SetupStreamRoutes(db *sqlx.DB) {
-	streamHandler := handler.NewStreamHandler(db)
+func SetupStreamRoutes(db *sqlx.DB, discordSesseion *discordgo.Session) {
+	streamHandler := handler.NewStreamHandler(db, discordSesseion)
 	http.HandleFunc("/streams/create", streamHandler.CreateStream)
 }
